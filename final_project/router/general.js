4f622615732aea480e18e8233d452264b2208d58
unknown <dorianekengni@gmail.com>
2025-03-05 12:05:14 +0200

    add logic to get review by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,6 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
   res.send(JSON.stringify({ books }, null, 4))
 });
 
@@ -23,10 +22,9 @@ public_users.get('/',function (req, res) {
     let book = books[key];
     if(book.isbn === param || book.author === param || book.title === param) {
       return book;
-    } else {
-      return `No book found`
     }
   }
+  return `No book found`
 }
 
 // Get book details based on ISBN
@@ -35,7 +33,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   const bookFoundWitnIsbn = findBookByParam(isbn)
   res.send(bookFoundWitnIsbn)
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
@@ -52,8 +50,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const findBookByIsbn = findBookByParam(isbn);
+
+  if(typeof findBookByIsbn == 'object'){
+    res.send(findBookByIsbn.review);
+  } else{
+    res.send('book review not found')
+  }
 });
 
 module.exports.general = public_users;
