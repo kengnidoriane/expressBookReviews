e18efbdc1c8afb4ff716545a614ee8f6972d3c9a
unknown <dorianekengni@gmail.com>
2025-03-05 11:26:25 +0200

    logic to getBookByTitle function

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,8 +45,9 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const bookFindByTitle = findBookByParam(title);
+  res.send(bookFindByTitle)
 });
 
 //  Get book review
