f8b3e5183a465e23e40d2247f7b17480378e532d
unknown <dorianekengni@gmail.com>
2025-03-05 18:03:17 +0200

    add logic for .cutomer/login endpoint

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const session = require('express-session');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{
+  "username": "manon",
+  "password": "manon#"
+}
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  //write code to check if username and password match the one we have in records.
+  const user = users.find(user => user.username === username && user.password === password);
+  return !!user;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if(!username || !password) {
+    return res.status(400).json({message: "Error loggging in: Username and password are required."});
+  }
+
+  if(authenticatedUser(username, password)) {
+   //Generate jwt
+    const token = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60});
+
+    req.session.authorization = {
+      token, username
+    }
+    return res.status(200).send("User Successfully logged in")
+  } else {
+      return res.status(401).json({message: "Invalid username or password." });
+
+    }
+
 });
 
 // Add a book review
